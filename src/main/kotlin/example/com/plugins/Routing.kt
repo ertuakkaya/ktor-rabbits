4a4afbcfad97682c39ca3cf79ff9151d29fd587a
ertuakkaya <ertuakkaya@gmail.com>
2024-07-10 13:43:46 +0300

    can reach rabbit image urls

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -5,17 +5,21 @@ import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
 
         randomRabbit()
 
-        get("/") {
-        }
+
         // Static plugin. Try to access `/static/index.html`
-        static("/static") {
+        static {
             resources("static")
         }
+
+
+
+
     }
 }
